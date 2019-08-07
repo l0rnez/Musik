46c84c71c149ba89db84f08717e9373cb3701fc3
Master Leon <46943567+zoccer99@users.noreply.github.com>
2019-08-07 15:58:35 +0200

    GUI mit fertiger progress bar

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,99 +1,52 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.ComponentOrientation;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import javax.swing.Action;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.event.MenuEvent;
-import javax.swing.event.MenuListener;
 
-import Main.Musikverwaltung;
-import data.data;
-import song.Song;
-import playlist.Playlist;
-import gui.AddMusic;
-
-public class MainFrame extends JFrame implements ActionListener{
-	private JButton bAktualisieren, bAddMusic;
+public class MainFrame extends JFrame{
+	private JButton bLinks, bRechts, bMitte,play,stop,pause;
 	private JLabel label1;
-	private JFrame jframe1;
-	private JTable tabelle, tabelleinterpreten, tabellegenre, tabellesuchergebnisse;
+	private JFrame jframe1,PlayerFrame;
+	private JTable tabelle;
 	private JPanel panelmenu, panelliste;
 	private JMenu songs, genre, interpreter, playlists;
 	private JMenuBar menubar;
 	private JTextField searchbar;
-	private JScrollPane scroll, scrollinterpreten, scrollgenre, scrollsuche;
-	
-	
-	
-	ArrayList<Song> alleshier = new ArrayList<Song>();
-	String[] [] daten = null;
-	
-	
-	//Funktionen im GUI
-	public String[] [] druckenTabelle(ArrayList<Song> neu) {
-		alleshier = neu;
-		ArrayList<String> titles = new ArrayList<String>();
-		ArrayList<String> genra = new ArrayList<String>();
-		ArrayList<String> interpreters = new ArrayList<String>();
-		
-		String[] [] daten = new String [alleshier.size()] [3];
-		
-		for(int i = 0; i < alleshier.size(); i++) {
-			titles.add(alleshier.get(i).getTitle());
-			genra.add(alleshier.get(i).getGenre());
-			interpreters.add(alleshier.get(i).getInterpreter());
-		}
-		
-		for(int i = 0; i < titles.size(); i++) {
-			daten[i] [0] = titles.get(i);
-			daten[i] [1] = interpreters.get(i);
-			daten[i] [2] = genra.get(i);
-		}
-		return daten;
-	}
-	
-	
-	
-	AddMusic frame2 = new AddMusic();  //könnte man eventuell anders lösen
-	
-	//alles Musik stücke
-	data alles = new data("Alles");
-	
-
-	
+	private JScrollPane scroll;
+	private JProgressBar fortschritt;
+	int songlänge=15;//Beispiel eig. über song.getlänge
+	Timer t = new Timer();
+	Timer t2 = new Timer();
+	boolean zpause=false;//Zwischenpause
 	
 	//Probleme:
 	//1. Warum zum fick ist das Fenster leer, wenn man es Ã¶ffnet, aber fÃ¼llt sich, wenn man es grÃ¶ÃŸer macht?
-	//2. Warum muss Actionlistener (ganz unten) geaddet werden, damit der sich nicht beschwert
-	//3. bei dem "Add"-Fenster muss man immer Enter drücken, um Titel und ähnliches zu bestätigen
-		//Lösung: Focus listener, der reagiert wenn man von dem Feld weggeht
-	//4. Tabelle lässt sich durch "Aktualisieren" nicht refreshen, obwohl daten, aus "data" in MainFrame ankomme
 	
 	
-	
-	
-	
-	public MainFrame(){
+	public MainFrame()
+	{
 		setLayout(null);
 		setVisible(true);
 		setSize(800, 600);
@@ -103,59 +56,14 @@ public class MainFrame extends JFrame implements ActionListener{
 		//setResizable(false);    //an sich gut, aber geht nicht, wenn die Komponenten nicht auftauchen
 		
 		
-	//Testtitel
-	alles.contentAdd("Keck", "Kecker", "Keckno");
-	alles.contentAdd("Klang", "White House Mouse", "Melodic Hardcore Punk");
-	alles.contentAdd("Suppenparty", "Rentner-Rocker", "Schlager");
 		
 		
-		
-		
-	//Tabelle Alles
-	String[] titlesTable = {"Titel","Interpreten","Genre"};
-	alleshier = alles.getContent();
-	daten = druckenTabelle(alleshier);
+	//Tabelle test
+	String[] titles = {"Titel","Interpreten","Genre"};
 	
 	
-	
-	//Tabelle Interpreten
-	String[] interpretentitel = {"Interpreten"};
-	String[] [] datainterpreten = new String[alleshier.size()] [1];
-	
-	for(int i = 0; i < alleshier.size(); i++) {
-		datainterpreten[i] [0] = alleshier.get(i).getInterpreter();
-	}
-
-	//Tabelle Genre
-	String[] genratitel = {"Genre"};
-	String[] [] datagenre = new String[alleshier.size()] [1];
-	
-	for(int i = 0; i < alleshier.size(); i++) {
-		datagenre[i] [0] = alleshier.get(i).getGenre();
-	}
-		
-	
-	//Tabelle suchen
-/*	String suche;
-	String [] suchetitel = {"Suchergebnisse"};
-	String[] [] sucheergebnisse = new String[alleshier.size()] [1];
-	ArrayList<Song> gesuchteTitel = new ArrayList<Song>();
-	ArrayList<String> gesuchteInterpreten = new ArrayList<String>();
-	ArrayList<String> gesuchteGenre = new ArrayList<String>();
-	
-	
-	
-	
-	for(int i = 0; i < alleshier.size(); i++) {
-		sucheergebnisse [i] [0] = gesuchteTitel.get(i).getTitle();
-	}
-*/
-
-	
-
-		
-		
-		
+	Object[] [] data = {{"Peters Kackhaus", "Peter der Kacker", "Deathmetal"}, {"All the leaves are white", "White Powerranger", "Melodic Hardcore Punk"}};
+	//Zahlen automatisch erzeugen	
 		
 		
 		
@@ -186,44 +94,136 @@ public class MainFrame extends JFrame implements ActionListener{
 			
 			playlists = new JMenu("Playlists");
 			playlists.setFont(f1);
-			
-			bAddMusic = new JButton("Add");
-			bAddMusic.setFont(f1);
-			bAddMusic.setAlignmentX(Component.LEFT_ALIGNMENT); //macht nichts, Knopf soll nach ganz links
 		
-			bAktualisieren = new JButton("Aktualisieren");
-			bAktualisieren.setFont(f1);
+		
 		
 			//Searchbar
 			searchbar = new JTextField("Was möchten sie suchen?");
 			//mouselistener damit bei klick der Text "was mchten sie..." verschwindet
 			
-			
-			
-			
-			
-		//Songtabellen
-		tabelle = new JTable(daten, titlesTable);
-		tabelleinterpreten = new JTable(datainterpreten, interpretentitel);
-		tabellegenre = new JTable(datagenre, genratitel);
-		//tabellesuchergebnisse = new JTable(sucheergebnisse,suchetitel);
+		//Songtabelle
+		tabelle = new JTable(data, titles);
+		tabelle.setBounds(10, 50, 500, 500);
 		
 		scroll = new JScrollPane(tabelle);
 		scroll.setBounds(10, 50, 760, 400);
-		scroll.setVisible(true);
 		
-		scrollinterpreten = new JScrollPane(tabelleinterpreten);
-		scrollinterpreten.setBounds(10, 50, 760, 400);
-		scrollinterpreten.setVisible(false);
 		
-		scrollgenre = new JScrollPane(tabellegenre);
-		scrollgenre.setBounds(10, 50, 760, 400);
-		scrollgenre.setVisible(false);
+		//Player
+		JPanel Player = new JPanel();
+		Player.setBounds(10, 450, 760, 80);
+		Player.setLayout(new FlowLayout());
+		Player.setBackground(Color.white);
 		
-		scrollsuche = new JScrollPane(tabellesuchergebnisse);
-		scrollsuche.setBounds(10, 50, 760, 400);
-		scrollsuche.setVisible(false);
+		fortschritt = new JProgressBar(0,songlänge);	//playleiste
+		fortschritt.setValue(0);
+		fortschritt.setStringPainted(true);
+		fortschritt.setPreferredSize(new Dimension(480,50));
+		fortschritt.setSize(getPreferredSize());
 		
+		play = new JButton();
+		play.setOpaque(false);
+		play.setContentAreaFilled(false);
+		play.setBorderPainted(false);
+		play.setIcon(new ImageIcon("play.png"));
+		play.addActionListener(new ActionListener() 
+		{
+			
+				public void actionPerformed(ActionEvent ae)
+				{
+					t = new Timer();
+					if(!zpause)
+					{
+							if(fortschritt.getValue()<=songlänge)
+							{
+								t.scheduleAtFixedRate(new TimerTask() 
+								{
+	
+									@Override
+									public void run() 
+									{
+										fortschritt.setValue(fortschritt.getValue()+1);
+										System.out.println(fortschritt.getValue());
+										if(fortschritt.getValue() == songlänge)
+										{
+											fortschritt.setValue(0);
+											t.cancel();
+										}
+									}
+									
+								}, fortschritt.getValue(), 1000);
+							}
+			
+					}
+					
+						if(zpause)
+						{
+							t2 = new Timer();
+							if(fortschritt.getValue()<=songlänge)
+							{
+								
+								t2.scheduleAtFixedRate(new TimerTask() 
+								{
+	
+									@Override
+									public void run() 
+									{
+										fortschritt.setValue(fortschritt.getValue()+1);
+										System.out.println(fortschritt.getValue());
+										
+										if(fortschritt.getValue() == songlänge)
+										{
+											fortschritt.setValue(0);
+											t2.cancel();
+										}
+									}
+									
+								}, fortschritt.getValue(), 1000);
+							
+						
+							}
+						}
+				}
+		});
+		
+		pause = new JButton();
+		pause.setOpaque(false);
+		pause.setContentAreaFilled(false);
+		pause.setBorderPainted(false);
+		pause.setIcon(new ImageIcon("pause.png"));
+		pause.addActionListener(new ActionListener()
+				{
+					
+					public void actionPerformed(ActionEvent ae)
+					{
+						zpause=true;
+						t.cancel();
+						t2.cancel();
+					}
+				});
+		
+		stop = new JButton();
+		stop.setOpaque(false);
+		stop.setContentAreaFilled(false);
+		stop.setBorderPainted(false);
+		stop.setIcon(new ImageIcon("stop.png"));
+		stop.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent ae)
+			{
+				fortschritt.setValue(0);
+				t.cancel();
+				t2.cancel();
+			}
+		});
+		
+	
+		
+		Player.add(play);
+		Player.add(pause);
+		Player.add(fortschritt);
+		Player.add(stop);
+		//Player.add(shuffle,BorderLayout.WEST );
 		
 		
 		
@@ -233,131 +233,17 @@ public class MainFrame extends JFrame implements ActionListener{
 		menubar.add(playlists);
 		menubar.add(genre);
 		menubar.add(interpreter);
-		menubar.add(bAddMusic);
-		menubar.add(bAktualisieren);
 		
-		
-		panelmenu.add(bAddMusic);
 		panelmenu.add(menubar);
 		panelmenu.add(searchbar);
-		panelmenu.add(bAktualisieren);
-		
 		
 		
 		
 		//panelliste.add(tabelle);
-		add(scrollsuche);
+		
 		add(scroll);
-		add(scrollinterpreten);
-		add(scrollgenre);
 		add(panelmenu);
-		
-		
-		
-		//Button Actions
-		bAddMusic.addActionListener(new java.awt.event.ActionListener() {
-
-			public void actionPerformed(ActionEvent e) {
-				frame2.setVisible(true);
-				alles.testDruck();
-			}
-			
-		});
-		
-		
-		//Menu Actions
-		genre.addMenuListener(new MenuListener() {
-			
-			public void menuCanceled(MenuEvent arg0) {
-				
-			}
-
-			
-			public void menuDeselected(MenuEvent arg0) {
-				scroll.setVisible(true);
-				scrollgenre.setVisible(false);
-				
-			}
-
-			
-			public void menuSelected(MenuEvent arg0) {
-				scroll.setVisible(false);
-				scrollgenre.setVisible(true);
-				
-			}	
-		});
-		
-		
-		interpreter.addMenuListener(new MenuListener() {
-
-			public void menuCanceled(MenuEvent e) {
-				
-			}
-			
-
-			@Override
-			public void menuDeselected(MenuEvent e) {
-				scroll.setVisible(true);
-				scrollinterpreten.setVisible(false);
-				
-			}
-			
-			@Override
-			public void menuSelected(MenuEvent e) {
-				scroll.setVisible(false);
-				scrollinterpreten.setVisible(true);;
-				
-			}
-		});
-		
-		
-		
-		//Searchbar Action
-		searchbar.addKeyListener(new KeyListener() {
-			
-			@Override
-			public void keyTyped(KeyEvent e) {
-				/*char a = e.getKeyChar();
-				suche = Character.toString(a);
-				gesuchteTitel = alles.searchSong(suche);*/
-				
-			}
-			
-			@Override
-			public void keyReleased(KeyEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void keyPressed(KeyEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		bAktualisieren.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				alles.testDruck();
-				daten = druckenTabelle(data.getContent());
-				tabelle.repaint();
-				
-			}
-		});
-		
+		add(Player);
 		
 	}
-	
-	
-	
-
-
-	//Warum muss das geaddet werden, damit der sich nicht beschwert
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
